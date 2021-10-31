ff62b0464cb76027ec0ed400124c696dca457c63
LinddM <delaroca.estrada21@gmail.com>
2021-10-31 13:10:32 -0600

    pyramid usage

--- laboratorio_2.py
+++ laboratorio_2.py
@@ -1,39 +1,44 @@
 # # no hay que hacer sliding window
-from video import CountsPerSec, VideoCaptureThread
+from video import VideoCaptureThread
 import cv2 as cv
 import numpy as np
 
 def template_match(frame, template, w, h):
-    """ Annotate an image with text
+    """ 
     """
     imageGray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     res = cv.matchTemplate(imageGray, template, cv.TM_CCOEFF_NORMED)
-    loc = np.where(res >= 0.4)
+    loc = np.where(res >= 0.5)
 
     for pt in zip(*loc[::-1]):
-        cv.rectangle(frame, pt, (pt[0] + w, pt[1] + h), (0, 255, 0), 3)
+        cv.rectangle(frame, pt, (pt[0] + w, pt[1] + h), (0, 255, 0), 2)
+
+def pyramid(img, scale=0.7, min_size=(50, 50)):
+    yield img
+    
+    while True:
+        img = cv.resize(img, None,fx=scale, fy=scale, interpolation = cv.INTER_CUBIC)
+        if ((img.shape[0]<min_size[0]) and img.shape[1]<min_size[1]):
+            break
+        yield img
 
 def captureThread(source=0):
     """
     """
-
-    template = cv.imread('Logo-UFM-50-anios-01-2.png', 0)
-    w, h = template.shape[::-1]
+    template = cv.resize(cv.imread('Logo-UFM-50-anios-01-2.png', 0), (618, 584), interpolation=cv.INTER_LINEAR)
     capThreaded = VideoCaptureThread(source).start()
-    cps = CountsPerSec().start()
-
     while True:
         frame = capThreaded.frame
         if not capThreaded.ret or cv.waitKey(1) == ord("q"):
             capThreaded.stop()
             break
         
-        temp_small = cv.resize(template, (618, 584), interpolation=cv.INTER_LINEAR)
-        template_match(frame, temp_small, w, h)
-        cv.imshow("CAPTURE_THREAD", frame)
+        for temp in pyramid(template):
+            w, h = temp.shape[::-1]
+            template_match(frame, temp, w, h)
+        cv.imshow("Logo detection", frame)
 
 
 if __name__ == '__main__':
     filename = 0
-    
     captureThread(filename)
--- video.py
+++ video.py
@@ -25,29 +25,3 @@ class VideoCaptureThread:
 
     def stop(self):
         self.stopped = True
-
-
-class CountsPerSec:
-    """ Measure performance in frames per second
-    """
-
-    def __init__(self):
-        self._start_t = None
-        self._frames = 0
-
-    def start(self):
-        self._start_t = datetime.now()
-        return self
-    
-    def increment(self):
-        self._frames += 1
-    
-    def freq(self):
-        elapsed_time = (datetime.now() - self._start_t).total_seconds()
-
-        if elapsed_time > 0:
-            f =  self._frames/elapsed_time
-        else:
-            f = 0
-
-        return f
