dc661160326d29085ae52173e1de1a679c63525d
LinddM <delaroca.estrada21@gmail.com>
2021-10-31 00:28:22 -0600

    template matching without pyramids

--- laboratorio_2.py
+++ laboratorio_2.py
@@ -1,20 +1,24 @@
-# no hay que hacer sliding window
+# # no hay que hacer sliding window
+from video import CountsPerSec, VideoCaptureThread
 import cv2 as cv
-from video import *
+import numpy as np
 
-def img_annotate(img, text):
+def template_match(frame, template, w, h):
     """ Annotate an image with text
     """
-    cv.putText(img, text,(10, 20), cv.FONT_HERSHEY_SIMPLEX, 0.50, (0, 255, 0))
-    # # Maybeee
-    # PONER ACA EL TEMPLATE MATCHING (CUADRITO VERDE)
-    # HACER ACA LAS PIRAMIDES
-    return img
+    imageGray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    res = cv.matchTemplate(imageGray, template, cv.TM_CCOEFF_NORMED)
+    loc = np.where(res >= 0.4)
+
+    for pt in zip(*loc[::-1]):
+        cv.rectangle(frame, pt, (pt[0] + w, pt[1] + h), (0, 255, 0), 3)
 
 def captureThread(source=0):
     """
     """
 
+    template = cv.imread('Logo-UFM-50-anios-01-2.png', 0)
+    w, h = template.shape[::-1]
     capThreaded = VideoCaptureThread(source).start()
     cps = CountsPerSec().start()
 
@@ -23,45 +27,13 @@ def captureThread(source=0):
         if not capThreaded.ret or cv.waitKey(1) == ord("q"):
             capThreaded.stop()
             break
-        fps = str(round(cps.freq(),2))
-        frame = img_annotate(frame, fps)
         
+        temp_small = cv.resize(template, (618, 584), interpolation=cv.INTER_LINEAR)
+        template_match(frame, temp_small, w, h)
         cv.imshow("CAPTURE_THREAD", frame)
-        cps.increment()
+
 
 if __name__ == '__main__':
     filename = 0
     
     captureThread(filename)
-
-##### Template matching
-
-# import cv2 as cv
-# import numpy as np
-# from matplotlib import pyplot as plt
-# img = cv.imread('lenna.png',0)
-# img2 = img.copy()
-# template = cv.imread('lenna_por.png',0)
-# w, h = template.shape[::-1]
-# # All the 6 methods for comparison in a list
-# methods = ['cv.TM_CCOEFF', 'cv.TM_CCOEFF_NORMED', 'cv.TM_CCORR',
-#             'cv.TM_CCORR_NORMED', 'cv.TM_SQDIFF', 'cv.TM_SQDIFF_NORMED']
-# for meth in methods:
-#     img = img2.copy()
-#     method = eval(meth)
-#     # Apply template Matching
-#     res = cv.matchTemplate(img,template,method)
-#     min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
-#     # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-#     if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
-#         top_left = min_loc
-#     else:
-#         top_left = max_loc
-#     bottom_right = (top_left[0] + w, top_left[1] + h)
-#     cv.rectangle(img,top_left, bottom_right, 255, 2)
-#     plt.subplot(121),plt.imshow(res,cmap = 'gray')
-#     plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
-#     plt.subplot(122),plt.imshow(img,cmap = 'gray')
-#     plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
-#     plt.suptitle(meth)
-#     plt.show()
